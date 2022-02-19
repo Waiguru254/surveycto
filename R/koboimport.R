d0cd645227fa6e4ec9796f9f1fe3641b45fde121
Waiguru254 <waigurusamuel@gmail.com>
2022-02-19 14:38:07 +0300

    Updating koboimport function

--- R/koboimport.R
+++ R/koboimport.R
@@ -1,7 +1,7 @@
 ### Importing data function
 #' Importing data from Kobo tool box (even Humanitarian Account)
-#' KoBoToolBox provides a suites of tools to collect data in challenging
-#' environment. It is free and opne source and work both online and offline.
+#'       KoBoToolBox provides a suites of tools to collect data in challenging
+#'       environment. It is free and opne source and work both online and offline.
 #'
 #' KoboTool Box also provides advanced feature and make it useful in
 #' advanced scenarios.It provide data access through RESTAPI, which
@@ -11,7 +11,7 @@
 #'
 #' @param servername either 'kobo.humanitarianresponse.info/' or
 #'                  'kc.kobotoolbox.org' However, the current function is
-#'                  build majorly to stream data from kobotoolbox.org.
+#'                  largely build to stream data from kobotoolbox.org.
 #'
 #' @param formid  To download data, a unique identifier of the form is needed
 #'                   to construct a URL used to fetch data. The fetch data
@@ -32,25 +32,83 @@
 #' data<-koboimport(servername,form_id,username,password)
 #'
   koboimport<- function (servername,formid, username,password) {
+
+    ### Loading dplyr, which is used in this function.
     suppressMessages(library(dplyr))
+
+    ### Check whether you are connected to the internet
+
+    if(isFALSE(curl::has_internet())) {
+
+        stop(paste("You are offline. Connect to the internet."), call. = FALSE)
+
+      }
+
+    ### Checking the validity of servername
+    if (nchar(servername)!=15 & grepl("kobotoolbox",servername)) {
+
+        stop(paste("Kindly use \"kobotoolbox.org\" as your servername"), call. = FALSE)
+
+      }
+    ###initializing the data before fetching
     request<-c() ; data_cto<-c() ## Removing the chances of previous data
+
+    ### Fetching data from the server
+    ###Creating the url from the details provided to the function (Servername and formID)
+
     urls<-paste("https://kf.",servername,"/assets/",formid,"/submissions/?format=json",sep='')
-    print(urls)
+
+    ### Authenticate the access to the server, and fetching the data in form of json
+
     rawdata<-httr::GET(urls,httr::authenticate(username,password))
+
+    #### Converting json data into characters
+
     d_content <- rawToChar(rawdata$content)
     d_content <- jsonlite::fromJSON(d_content)
+
+    ### Checking whether logins are valid
+    if (grepl("username/password",d_content[1])) {
+
+      stop(paste("Invalid Username or password."), call. = FALSE)
+
+    }
+
+    ### Converting the data into dataframe for processing
+
     data_cto <-suppressWarnings(suppressMessages(as.data.frame(d_content)))
-    print(paste(nrow(data_cto),"of",formid, "have been downloaded.",sep=' '))
+
+    ### Printing the number of submission(s) download form the server
+
+    cat(crayon::green(paste(nrow(data_cto),"submissions of",formid, " form have been downloaded.",sep=' ')))
+
+    #### Dropping the repeat group variables(This will be include in updated version of the funtion, going forward)
+
     df_new <- data_cto %>% dplyr:: select(-contains("]/")) # Dropping repeat group data.
-    df_names<-gsub('/_','_',gsub("(\\d+)$", "_\\1",gsub('-','',names(df_new)))) ### Removing group names
-    df_namesd<- sub('.*/','', df_names) ### Further cleaning of column names
-    colnames(df_new)<-df_namesd
-    ###Formatting the data
+
+    ### Removing the repeat group column names for re-labelling of the data.frame
+
+    df_name<-gsub('/_','_',gsub("(\\d+)$", "_\\1",gsub('-','',names(df_new)))) ### Removing group names
+
+    df_names<- sub('.*/','', df_name) ### Further cleaning of column names
+
+    ###Adding column names to the processed data
+
+    colnames(df_new)<-df_names
+
+    ###Formatting the data for exporting
     df_new[] <- lapply(df_new, gsub, pattern='n/a', replacement='NA')
-    df_new[] <- lapply(df_new, gsub, pattern='True', replacement='1')
-    df_new[] <- lapply(df_new, gsub, pattern='False', replacement='0')
+
+    ### Binary variables are fetched as true/ false. Converting them to binary code(0,1)
+    ### to ease the process of processing and analysis.
+
+    df_new[] <- lapply(df_new, gsub, pattern='True', replacement='1') ## for true
+    df_new[] <- lapply(df_new, gsub, pattern='False', replacement='0') ## for false
+
+    ### List of items to returned by the function.
     return(df_new)
+    return(urls)
+    return(df_names)
+
   }
 
-
-  #data<- koboimport('kobotoolbox.org','a595eJvpxXHATXpJqR8GdM','lytons_analytica','Nyakingei#52#')
