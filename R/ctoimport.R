2536b16105d3a36859f5d225e18753cfef9e881e
Waiguru Analyst <39795674+Waiguru254@users.noreply.github.com>
2022-06-10 17:33:09 +0300

    Update ctoimport.R
    bracketing

--- R/ctoimport.R
+++ R/ctoimport.R
@@ -227,9 +227,8 @@
         na.omit(name)%>%
         mutate(value_s=paste(paste('\"',gsub(paste0(".*?($|'|", paste(paste0("\\",
                       keeps_labs), collapse = "|"), "|[^[:punct:]]).*?"), "\\1", label_rawd),
-                                   '\"', sep=""),name,sep = " = "))
-        choices_processed <- choices_processed %>%
-        dplyr::select(c('list.name','value_s') %>%
+                                   '\"', sep=""),name,sep = " = "))%>%
+        dplyr::select(c('list.name','value_s')) %>%
         group_by_at(vars('list.name')) %>%
         summarize_all(paste, collapse=",")
         
