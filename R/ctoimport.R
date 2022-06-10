ccc6294d3aee398bd88c82587ee22f8f3fd1b822
Waiguru Analyst <39795674+Waiguru254@users.noreply.github.com>
2022-06-10 14:21:40 +0300

    Update ctoimport.R
    choices

--- R/ctoimport.R
+++ R/ctoimport.R
@@ -213,8 +213,8 @@
               }
        } 
  # colnames(survey)[colnames(survey)==lab_lang]<-"label_rawd"
-  colnames(choices)[colnames(choices)==lab_langc]<-"label_rawd"
-  colnames(choices)[grepl("list.", colnames(choices))] <- "list.name"
+  colnames(choices)[colnames(choices)==lab_langc]<-"list.name"
+ # colnames(choices)[grepl("list.", colnames(choices))] <- "list.name"
 
       ### Symbols to keep in choices labels
       keeps_labs <- c("+",'[',']','(',')',"?","_",'/')
